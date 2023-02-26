6177bbe9dbd0fd513238deec4032381297a60b66
ssaneg <123612577+ssaneg@users.noreply.github.com>
2023-02-26 15:32:48 -0800

    Add files via upload

---
+++ sentimental1 (1).py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+ds = pd.read_csv("/Users/satyasanegepalli/Downloads/Tweets.csv")
+
+#deleting null values
+bool_series = pd.isnull(ds["text"])
+length = len(bool_series)
+i=1
+while i<length:
+    if bool_series[i]=="False":
+        ds.drop(i,axis=0,inplace=True)
+
+#tag_id
+ds['tag_id'] = ds['sentiment']
+for i in ds.index:
+    if ds['sentiment'][i] == "neutral":
+        ds['tag_id'][i] = 0
+    elif ds['sentiment'][i] == "positive":
+        ds['tag_id'][i] = 1
+    elif ds['sentiment'][i] == "negative":
+        ds['tag_id'][i] = 2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
